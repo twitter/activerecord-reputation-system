4a8ffaab44db1345e82232d8f2b3c93ddcd72265
Katsuya Noguchi <katsuya@twitter.com>
2012-08-10 01:25:56 +0900

    Fix a migration class name

--- lib/generators/reputation_system/templates/change_reputation_messages_index_to_unique.rb
+++ lib/generators/reputation_system/templates/change_reputation_messages_index_to_unique.rb
@@ -14,7 +14,7 @@
 #  limitations under the License.
 ##
 
-class ChangeReputationMessagesIndexUnique < ActiveRecord::Migration
+class ChangeReputationMessagesIndexToUnique < ActiveRecord::Migration
   def self.up
     remove_index :rs_reputation_messages, :name => "index_rs_reputation_messages_on_receiver_id_and_sender"
     add_index :rs_reputation_messages, [:receiver_id, :sender_id, :sender_type], :name => "index_rs_reputation_messages_on_receiver_id_and_sender", :unique => true
