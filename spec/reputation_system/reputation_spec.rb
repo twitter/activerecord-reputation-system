49a907c814db6d867ff131edef5a09d375d8cbc1
Sumit Shah <shah@twitter.com>
2012-09-09 21:57:52 -0700

    incorporate code review comments

--- spec/reputation_system/reputation_spec.rb
+++ spec/reputation_system/reputation_spec.rb
@@ -159,4 +159,35 @@ describe ActiveRecord::Base do
       end
     end
   end
+
+  context "Contribution" do
+    describe "#contribution_value_for" do
+      it "should return 1 as if there is no data" do
+        @question.contribution_value_for(:total_votes).should == 1
+      end
+
+      it "should return appropriate value in case of valid input" do
+        question2 = Question.create!(:text => 'Does this work too?', :author_id => @user.id)
+        question3 = Question.create!(:text => 'Does this work too?', :author_id => @user.id)
+        @question.add_evaluation(:total_votes, 1, @user)
+        question2.add_evaluation(:total_votes, 2, @user)
+        question3.add_evaluation(:total_votes, 3, @user)
+        @question.contribution_value_for(:total_votes).should == 1.to_f/6
+        question2.contribution_value_for(:total_votes).should == 0.5
+        question3.contribution_value_for(:total_votes).should == 1
+      end
+
+      it "should raise exception if invalid reputation name is given" do
+        lambda {@question.contribution_value_for(:invalid)}.should raise_error(ArgumentError)
+      end
+
+      it "should raise exception if scope is given for reputation with no scopes" do
+        lambda {@question.contribution_value_for(:difficulty, :s1)}.should raise_error(ArgumentError)
+      end
+
+      it "should raise exception if scope is not given for reputation with scopes" do
+        lambda {@phrase.contribution_value_for(:difficulty_with_scope)}.should raise_error(ArgumentError)
+      end
+    end
+  end
 end
